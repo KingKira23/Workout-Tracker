c4218f3bbbaac40db50c1af88fd418ecc367cbf8
Alberto Ruiz <alberto.ruiz.iii@gmail.com>
2020-03-25 23:30:24 -0500

    stopping here for tonight

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,12 @@ const db = require("../models/Workout")
 module.exports = function(app) {
     app.post("/api/workouts", function(req, res) {
         console.log(req.body)
-        // db.create(req.body).then(function(Workout) {
-        //     res.json(Workout);
-        // })
+        db.create(req.body).then(function(Workout) {
+            res.json(Workout);
+        })
     })
 
-    app.put("/api/workouts/:WorkOutId", function(req, res) {
+    app.put("/api/workouts/:WorkoutId", function(req, res) {
         db.findOne({where: {id: req.params.id } }).then(function(response) {
             res.json(response.dataValues)
         })
